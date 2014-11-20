ae6f9f6ce326c42b7393ee64c34996fe32469baf
Daniel Lobaton <Dlobaton2@gmail.com>
2014-11-20 12:11:14 -0500

    Touched up Circles and Lines

--- Lab06.java
+++ Lab06.java
@@ -25,9 +25,6 @@ public class Lab06 extends Applet
 			g.drawLine(x1,y1,x2,y2);
 		}
 
-
-				
-		
 		// Draw Random Squares
 
 		for(int x = 1; x <=100 ; x++)
@@ -44,14 +41,14 @@ public class Lab06 extends Applet
 		}
 		
 		// Draw Random Circles
-		for(int x = 1; x <= 100; x++)
+	for(int x = 1; x <= 100; x++)
 		{
 			int c1 = (int)(Math.random() *(255));
 			int c2 = (int)(Math.random() *(255));
 			int c3 = (int)(Math.random() *(255));
-			int x1 = (int)(Math.random() *(300)+5);
-			int y1 = (int)(Math.random() *(200)+300);
-			int sz = (int)(Math.random() *120)-15;
+			int x1 = (int)(Math.random() *(300)+15);
+			int y1 = (int)(Math.random() *(200)+310);
+			int sz = (int)(Math.random() *100)-(5*3);
 			g.setColor(new Color(c1,c2,c3));
 			g.drawOval(x1, y1, sz, sz);
 		}
