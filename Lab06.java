a05ad142e14adc4f8198a097ee4c5be595a989d3
Daniel Lobaton <Dlobaton2@gmail.com>
2014-11-20 22:55:11 -0500

    Done with circles

--- Lab06.java
+++ Lab06.java
@@ -40,21 +40,29 @@ public class Lab06 extends Applet
 			
 		}
 		
-		//Fix alignment
+		//Draw Circles
 		for(int x = 1; x <= 100; x++)
 		{
 			int c1 = (int)(Math.random() *(255));
 			int c2 = (int)(Math.random() *(255));
 			int c3 = (int)(Math.random() *(255));
-			int x1 = (int)(Math.random() *(200)+9);
-			int y1 = (int)(Math.random() *(200)+250);
 			int sz = (int)(Math.random() * 200);
+			int x1 = (int)(Math.random() *(200)+9); //200
+			int y1 = (int)(Math.random() *(90)+300); //250
+			
 			g.setColor(new Color(c1,c2,c3));
 			g.drawOval(x1, y1, sz, sz);
 		}
 
 		// Draw 3-D Box
-		//Polygon penta = new Polygon();
+		Polygon penta = new Polygon();
+		penta.addPoint(456,506); //626
+		penta.addPoint(326,506); //756
+		penta.addPoint(466,456);
+		penta.addPoint(396,456);
+		
+		g.setColor(new Color(250,0,0));
+		g.fillPolygon(penta);
 	
 	}
 }
