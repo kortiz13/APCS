eeabda3aa45629e6098f1f45a6ea3f5209a8c710
Daniel Lobaton <Dlobaton2@gmail.com>
2014-11-21 00:08:35 -0500

    finished up circles + 3D effect polygon

--- Lab06.java
+++ Lab06.java
@@ -47,8 +47,8 @@ public class Lab06 extends Applet
 			int c2 = (int)(Math.random() *(255));
 			int c3 = (int)(Math.random() *(255));
 			int sz = (int)(Math.random() * 200);
-			int x1 = (int)(Math.random() *(200)+9); //200
-			int y1 = (int)(Math.random() *(90)+300); //250
+			int x1 = (int)(Math.random() *(200)+9);
+			int y1 = (int)(Math.random() *(90)+300);
 			
 			g.setColor(new Color(c1,c2,c3));
 			g.drawOval(x1, y1, sz, sz);
@@ -56,11 +56,30 @@ public class Lab06 extends Applet
 
 		// Draw 3-D Box
 		Polygon penta = new Polygon();
-		penta.addPoint(456,506); //626
-		penta.addPoint(326,506); //756
-		penta.addPoint(466,456);
-		penta.addPoint(396,456);
-		
+		penta.addPoint(675+35,425); //top right corner
+		penta.addPoint(625-35,425); //top left
+		penta.addPoint(625-35,425+100);
+		penta.addPoint(675+35,425+100);
+		Polygon penta1 = new Polygon();
+		penta1.addPoint(710,425);
+		penta1.addPoint(((700-600)*6)+65,375);
+		penta1.addPoint(((700-600)*6)+65,425);
+		Polygon penta2 = new Polygon();
+		penta2.addPoint(((700-600)*6)+65,375);
+		penta2.addPoint(((700-600)*6)+65,425);
+		penta2.addPoint(625-35,425);
+		penta2.addPoint(((700-600)*6)-55,375);
+		Polygon penta3 = new Polygon();
+		penta3.addPoint(((700-600)*6)-55,375);
+		penta3.addPoint((625-25)-10,425);
+		penta3.addPoint((625-25)-10,425+100);
+		penta3.addPoint(((700-600)*6)-55,475);
+		g.setColor(Color.green);
+		g.fillPolygon(penta3);
+		g.setColor(Color.yellow);
+		g.fillPolygon(penta2);
+		g.setColor(new Color(0,0,250));
+		g.fillPolygon(penta1);
 		g.setColor(new Color(250,0,0));
 		g.fillPolygon(penta);
 	
